578563c6fcc8d812fdd84acdc3ad2de1d80c93a2
Noah Brinkman <noahpbrinkman@gmail.com>
2023-07-04 11:21:41 +0200

    slightly less buggy

--- SuperLiminalCloneProject/Assets/Scripts/CharacterPickupHandler.cs
+++ SuperLiminalCloneProject/Assets/Scripts/CharacterPickupHandler.cs
@@ -10,15 +10,15 @@ using UnityEngine.UI;
 public class CharacterPickupHandler : MonoBehaviour
 {
     [SerializeField] private float _range;
-    [SerializeField] private LayerMask _layers;
+    [SerializeField] private LayerMask _pickupLayer;
+    [SerializeField] private LayerMask _placeLayers;
     [SerializeField] private Camera _cam;
-    [SerializeField] private Image _image;
-    [SerializeField] private float _pickupDistance;
-    [SerializeField] private float _offset;
-    [SerializeField] private GameObject _currentPickedUpObject;
+    [SerializeField] private Image _image; 
+    private GameObject _currentPickedUpObject;
     private Vector3 targetScale = new Vector3();
     private  float _savedScale =0f;
     private float _savedDistance = 0.0f;
+    private float savedMass;
     private Vector3 hitOffset;
     /// <summary>
     /// Cast a ray from the center of the creen, look if it hit an object the player can pick up
@@ -28,18 +28,21 @@ public class CharacterPickupHandler : MonoBehaviour
     {
          RaycastHit ray;
          Debug.DrawRay(_cam.transform.position, _cam.transform.forward*_range);
-         if(Physics.Raycast(_cam.transform.position,_cam.transform.forward, out ray, _range, _layers))
+         if(Physics.Raycast(_cam.transform.position,_cam.transform.forward, out ray, _range, _pickupLayer))
          {
              Debug.Log("Hit");
              _image.color = new Color(1, 1, 1, 0);
              if (Input.GetKeyDown(KeyCode.E) && _currentPickedUpObject == null)
              {
                  _currentPickedUpObject = ray.collider.gameObject;
+                 ray.collider.enabled = false;
                  _currentPickedUpObject.GetComponent<Rigidbody>().isKinematic = true;
+                 savedMass = _currentPickedUpObject.GetComponent<Rigidbody>().mass;
                  _savedScale = _currentPickedUpObject.transform.localScale.x;
                  targetScale = _currentPickedUpObject.transform.localScale;
                  _savedDistance = Vector3.Distance(transform.position, _currentPickedUpObject.transform.position);
                  hitOffset = ray.collider.transform.InverseTransformPoint(ray.point); 
+                 
              }
          }  
          else
@@ -53,7 +56,7 @@ public class CharacterPickupHandler : MonoBehaviour
     {
         RaycastHit ray;
         
-        if(Physics.Raycast(_cam.transform.position,_cam.transform.forward, out ray, Mathf.Infinity,~_layers))
+        if(Physics.Raycast(_cam.transform.position,_cam.transform.forward, out ray, Mathf.Infinity,_placeLayers))
         {
             Debug.Log(ray.transform.name);
             Vector3 pos;
@@ -65,11 +68,11 @@ public class CharacterPickupHandler : MonoBehaviour
 
             while (Physics.OverlapBox(pos, targetScale).Length > 0)
             {
-                pos -= _cam.transform.forward * _currentPickedUpObject.transform.localScale.x / 2;
+                pos -= _cam.transform.forward * .2f;
                     scale =   Vector3.Distance(transform.position, pos)/_savedDistance ;
                     targetScale = new Vector3(scale, scale, scale);
             }
-
+            
             _currentPickedUpObject.transform.localScale = _savedScale * targetScale;
             _currentPickedUpObject.transform.position = pos ;
 
@@ -85,6 +88,9 @@ public class CharacterPickupHandler : MonoBehaviour
             if(Input.GetKeyUp(KeyCode.E))
             {
                 _currentPickedUpObject.GetComponent<Rigidbody>().isKinematic = false;
+                _currentPickedUpObject.GetComponent<Rigidbody>().mass =
+                    savedMass * _currentPickedUpObject.transform.localScale.x / _savedScale;
+                _currentPickedUpObject.GetComponent<Collider>().enabled = true;
                 _currentPickedUpObject =null;
                 _savedScale = 0;
                 targetScale = new Vector3();

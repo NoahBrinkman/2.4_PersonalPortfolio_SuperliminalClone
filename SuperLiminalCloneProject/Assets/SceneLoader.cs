444c0691f77fe7c778d9c02ffcb28b147beabb1a
Noah <noahpbrinkman@gmail.com>
2023-07-09 12:50:12 +0200

    Set up for final puzzles

---
+++ SuperLiminalCloneProject/Assets/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
